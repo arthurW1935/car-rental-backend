12c10bffc827788b868055da3fa5ac1ae0d86887
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-05-01 00:14:15 +0530

    initial commit

---
+++ src/test/java/com/group9/carrentalbackend/CarRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.carrentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
