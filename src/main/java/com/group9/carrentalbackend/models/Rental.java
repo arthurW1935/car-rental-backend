10bf5785d4bfb30f7bb05685893aac8bcbed6928
Tushar Khurana <tusharkhurana526@gmail.com>
2024-05-01 01:45:22 +0530

    new commit

--- src/main/java/com/group9/carrentalbackend/models/Rental.java
+++ src/main/java/com/group9/carrentalbackend/models/Rental.java
@@ -1,4 +1,26 @@
 package com.group9.carrentalbackend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Vehicle vehicle;
+    @ManyToOne
+    private Customer customer;
+    private Date StartDate;
+    private Date EndDate;
+    private Long TotalCost;
 }
