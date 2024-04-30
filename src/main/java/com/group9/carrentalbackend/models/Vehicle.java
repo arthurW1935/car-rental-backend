3bad13c7ac251f2160adb1943d0d76c99112e35a
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-05-01 02:01:06 +0530

    resolved errors

--- src/main/java/com/group9/carrentalbackend/models/Vehicle.java
+++ src/main/java/com/group9/carrentalbackend/models/Vehicle.java
@@ -1,9 +1,6 @@
 package com.group9.carrentalbackend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,13 +15,13 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String manafacturer;
+    private String manufacturer;
     private String model;
     private Integer year;
     private String licensePlateNumber;
     private Double currentMileage;
     @ManyToOne
     private Branch branch;
-    private enum type;
-    private enum Status;
+    private Type type;
+    private Status status;
 }
