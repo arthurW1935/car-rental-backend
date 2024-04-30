a2ca535ca37a123cba24b76c61ed47a2e1fd4365
Souvik Kumar <souvik.cyc@gmail.com>
2024-05-01 02:31:52 +0530

    Implemented Employee Controller

--- src/main/java/com/group9/carrentalbackend/controllers/EmployeeController.java
+++ src/main/java/com/group9/carrentalbackend/controllers/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("employees")
+@RequestMapping("employee")
 public class EmployeeController {
 
     @GetMapping("/{id}")
