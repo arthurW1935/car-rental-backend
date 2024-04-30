fff401f1655d8f83877dcf6481756661cbb49cde
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-05-01 03:43:59 +0530

    implemented Branch controller

--- src/main/java/com/group9/carrentalbackend/controllers/BranchController.java
+++ src/main/java/com/group9/carrentalbackend/controllers/BranchController.java
@@ -1,4 +1,37 @@
 package com.group9.carrentalbackend.controllers;
 
+import com.group9.carrentalbackend.models.Branch;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/branch")
 public class BranchController {
+
+    @GetMapping
+    public Branch getBranchById(){
+        return null;
+    }
+
+    @GetMapping
+    public List<Branch> getAllBranch(){
+        return null;
+    }
+
+    @PostMapping
+    public Branch createBranch(@RequestBody Branch branch){
+        return null;
+    }
+
+    @PutMapping
+    public Branch updateBranch(@RequestBody Branch branch){
+        return null;
+    }
+
+    @DeleteMapping
+    public Branch deleteBranch(){
+        return null;
+    }
+
 }
