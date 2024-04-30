e568eb42f4470da254e6af254d0f018bfa9958c9
Souvik Kumar <souvik.cyc@gmail.com>
2024-05-01 02:17:23 +0530

    Implemented EmployeeController

--- src/main/java/com/group9/carrentalbackend/controllers/EmployeeController.java
+++ src/main/java/com/group9/carrentalbackend/controllers/EmployeeController.java
@@ -1,4 +1,37 @@
 package com.group9.carrentalbackend.controllers;
 
+import java.util.List;
+
+import com.group9.carrentalbackend.models.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        return null;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        return null;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        return null;
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        return null;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id){
+        return null;
+    }
 }
