027405fcfcf8342f1c6fdc6e3d1f48f522321efd
arthurW1935 <sourashis.23bcs10038@ms.sst.scaler.com>
2024-05-01 16:18:24 +0530

    updated BranchController

--- src/main/java/com/group9/carrentalbackend/controllers/BranchController.java
+++ src/main/java/com/group9/carrentalbackend/controllers/BranchController.java
@@ -9,29 +9,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/branch")
 public class BranchController {
-    @GetMapping
-    public Branch getBranchById(){
-        return null;
+
+    private final BranchService branchService;
+    public BranchController(BranchService branchService) {
+        this.branchService = branchService;
+    }
+
+    @GetMapping("/{id}")
+    public Branch getBranchById(@PathVariable Long id){
+        return branchService.getBranchById(id);
     }
 
     @GetMapping
     public List<Branch> getAllBranch(){
-        return null;
+        return branchService.getAllBranch();
     }
 
     @PostMapping
     public Branch createBranch(@RequestBody Branch branch){
-        return null;
+        return branchService.createBranch(branch);
     }
 
     @PutMapping
     public Branch updateBranch(@RequestBody Branch branch){
-        return null;
+        return branchService.updateBranch(branch);
     }
 
-    @DeleteMapping
-    public Branch deleteBranch(){
-        return null;
+    @DeleteMapping("/{id}")
+    public Branch deleteBranchById(@PathVariable Long id){
+        return branchService.deleteBranch(id);
     }
 
 }
