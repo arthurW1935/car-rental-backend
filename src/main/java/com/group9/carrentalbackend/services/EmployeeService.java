b79061aa092cdbe26253c0fbb1871d3087aad18d
Souvik Kumar <souvik.cyc@gmail.com>
2024-05-01 11:02:25 +0530

    Implemented Employee Service

--- src/main/java/com/group9/carrentalbackend/services/EmployeeService.java
+++ src/main/java/com/group9/carrentalbackend/services/EmployeeService.java
@@ -4,6 +4,7 @@ import com.group9.carrentalbackend.models.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeService {
     Employee getEmployeeById(Long id);
@@ -18,32 +19,44 @@ public interface EmployeeService {
 
         @Override
         public Employee getEmployeeById(Long id) {
-            return null;
+            Optional<Employee> employee = employeeRepository.findById(id);
+            if(employee.isEmpty()){
+                return null; // throw exception
+            }
+            return employee.get();
         }
 
         @Override
         public List<Employee> getEmployees() {
-            return null;
+            return employeeRepository.findAll();
         }
 
         @Override
         public Employee createEmployee(Employee employee) {
-            return null;
+            return employeeRepository.save(employee);
         }
 
         @Override
         public Employee updateEmployee(Employee employee) {
-            return null;
+            Optional<Employee> existingEmployee = employeeRepository.findById(employee.getId());
+            if(existingEmployee.isEmpty()){
+                return null; // throw exception
+            }
+            return employeeRepository.save(employee);
         }
 
         @Override
         public void deleteEmployee(Long id) {
-
+            Optional<Employee> existingEmployee = employeeRepository.findById(id);
+            if(existingEmployee.isEmpty()){
+                return; // throw exception
+            }
+            employeeRepository.deleteById(id);
         }
 
         @Override
         public List<Employee> getEmployeesByBranch(Long branchId) {
-            return null;
+            return employeeRepository.findByBranchId(branchId);
         }
     }
 }
