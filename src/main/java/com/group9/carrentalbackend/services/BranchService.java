5611b5cabab1e9c3ee2fda1b096b5a53bae5749b
Tushar Khurana <tusharkhurana526@gmail.com>
2024-05-01 11:24:11 +0530

    Implemented branch Service

--- src/main/java/com/group9/carrentalbackend/services/BranchService.java
+++ src/main/java/com/group9/carrentalbackend/services/BranchService.java
@@ -1,4 +1,14 @@
 package com.group9.carrentalbackend.services;
 
+import com.group9.carrentalbackend.models.Branch;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface BranchService {
+    public Branch getBranchById(Long id);
+    public List<Branch> getAllBranch();
+    public Branch createBranch(Branch branch);
+    public Branch updateBranch(Branch branch);
+    public Branch deleteBranch(Long id);
 }
---
+++ src/main/java/com/group9/carrentalbackend/services/SelfBranchService.java
@@ -0,0 +1,69 @@
+package com.group9.carrentalbackend.services;
+
+import com.group9.carrentalbackend.models.Branch;
+import com.group9.carrentalbackend.models.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SelfBranchService implements BranchService {
+
+    private final BranchRepository branchRepository = new BranchRepository();
+    private final EmployeeRepository employeeRepository = new EmployeeRepository();
+    public SelfBranchService(BranchRepository branchRepository, EmployeeRepository employeeRepository){
+        this.branchRepository = branchRepository;
+        this.employeeRepository = employeeRepository;
+    }
+    @Override
+    public Branch getBranchById(Long id) {
+        Optional<Branch> branch = branchRepository.findById(id);
+        if(branch.isEmpty()) {
+            throw new BranchNotFoundException(id, "Product not found");
+        }
+        return branch.get();
+    }
+
+    @Override
+    public List<Branch> getAllBranch() {
+        return branchRepository.findAll();
+    }
+
+    @Override
+    public Branch updateBranch(Branch branch) {
+        Optional<Branch> existingBranch = branchRepository.findById(branch.getId());
+
+        if(existingBranch.isEmpty()){
+            throw new BranchNotFoundException(branch.getId(), "Branch not found");
+        }
+
+        if (branch.getManager().getId() == null) {
+            Employee manager = branch.getManager();
+            Employee savedEmployee = employeeRepository.save(manager);
+            branch.setManager(savedEmployee);
+        }
+        else{
+            Optional<Employee> manager = employeeRepository.findById(branch.getManager().getId());
+            if(manager.isEmpty()){
+                throw new EmployeeNotFoundException(branch.getManager().getId(), "Employee not found");
+            }
+            branch.setManager(manager.get());
+        }
+
+        return branchRepository.save(branch);
+    }
+
+    @Override
+    public Branch deleteBranch(Long id) {
+        Optional<Branch> existingBranch = branchRepository.findById(id);
+
+        if(existingBranch.isEmpty()){
+            throw new BranchNotFoundException(id, "Branch not found");
+        }
+
+        branchRepository.delete(existingBranch.get());
+
+        return existingBranch.get();
+    }
+}
