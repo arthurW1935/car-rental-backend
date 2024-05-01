c59a5c3426b032a7e372fa7444d4a6e739b35005
Souvik Kumar <souvik.cyc@gmail.com>
2024-05-01 10:47:10 +0530

    Implemented Employee Service

--- src/main/java/com/group9/carrentalbackend/services/EmployeeService.java
+++ src/main/java/com/group9/carrentalbackend/services/EmployeeService.java
@@ -1,4 +1,49 @@
 package com.group9.carrentalbackend.services;
 
+import com.group9.carrentalbackend.models.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface EmployeeService {
+    Employee getEmployeeById(Long id);
+    List<Employee> getEmployees();
+    Employee createEmployee(Employee employee);
+    Employee updateEmployee(Employee employee);
+    void deleteEmployee(Long id);
+    List<Employee> getEmployeesByBranch(Long branchId);
+
+    @Service
+    class EmployeeServiceImpl implements EmployeeService {
+
+        @Override
+        public Employee getEmployeeById(Long id) {
+            return null;
+        }
+
+        @Override
+        public List<Employee> getEmployees() {
+            return null;
+        }
+
+        @Override
+        public Employee createEmployee(Employee employee) {
+            return null;
+        }
+
+        @Override
+        public Employee updateEmployee(Employee employee) {
+            return null;
+        }
+
+        @Override
+        public void deleteEmployee(Long id) {
+
+        }
+
+        @Override
+        public List<Employee> getEmployeesByBranch(Long branchId) {
+            return null;
+        }
+    }
 }
