cedbd0d285d87d141e13e2a8a1b2cd7a77552a48
garvit jain <garvit.23bcs10091@ms.sst.scaler.com>
2024-05-01 15:09:03 +0530

    Update VehicleController.java

--- src/main/java/com/group9/carrentalbackend/controllers/VehicleController.java
+++ src/main/java/com/group9/carrentalbackend/controllers/VehicleController.java
@@ -2,45 +2,52 @@ package com.group9.carrentalbackend.controllers;
 
 import java.util.List;
 
+import com.group9.carrentalbackend.services.VehicleService;
+import org.springframework.web.bind.annotation.*;
 
 import com.group9.carrentalbackend.models.Vehicle;
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
+    private final VehicleService vehicleService;
+
+    VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
 
     @GetMapping("/{id}")
     public Vehicle getVehicleById(@PathVariable Long id) {
-        return null;
+        return vehicleService.getVehicleById(id);
     }
 
     @GetMapping("")
     public List<Vehicle> getAllVehicles() {
-        return null;
+        return vehicleService.getAllVehicles();
     }
 
     @PostMapping("")
     public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
-        return null;
+        return vehicleService.createVehicle(vehicle);
     }
 
     @PutMapping("/{id}")
     public Vehicle updateVehicleById(@RequestBody Vehicle vehicle) {
-        return null;
+        return vehicleService.updateVehicleById(vehicle);
     }
 
     @DeleteMapping("/{id}")
-    public void removeVehicleById(@PathVariable Long id) {
-        return;
+    public Vehicle removeVehicleById(@PathVariable Long id) {
+        return vehicleService.removeVehicleById(id);
     }
 
     @GetMapping("/type")
-    public List<Vehicle> getVehiclesByType(@RequestBody Type type) {
-        return null;
+    public List<Vehicle> getVehiclesByType(@RequestParam String type) {
+        return vehicleService.getVehiclesByType(type);
     }
 
     @GetMapping("/branch/{id}")
     public List<Vehicle> getVehiclesByBranch(@PathVariable Long id) {
-        return null;
-    }
+        return vehicleService.getVehiclesByBranch(id);
+    }
 }
